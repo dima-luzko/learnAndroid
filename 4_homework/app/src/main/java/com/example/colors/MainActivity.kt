de9310553f8273a48684eea0a2be6538c8ec8d01
Дима Лузько <dim.luzko@gmail.com>
2021-03-16 21:25:44 +0300

    Hide systemUI.

---
+++ 4_homework/app/src/main/java/com/example/colors/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.colors
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        hideSystemUI()
+    }
+
+    @Suppress("DEPRECATION")
+    private fun hideSystemUI() {
+        if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.R) {
+            window.decorView.apply {
+                systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+            }
+        }
+    }
+}
+
