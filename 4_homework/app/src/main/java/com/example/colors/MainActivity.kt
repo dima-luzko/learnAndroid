e9ca74f0b3ea0c05342cfbfc0bdd177c50c03901
Дима Лузько <dim.luzko@gmail.com>
2021-03-17 00:00:02 +0300

    Add bottomNavigationView and change icon.

--- 4_homework/app/src/main/java/com/example/colors/MainActivity.kt
+++ 4_homework/app/src/main/java/com/example/colors/MainActivity.kt
@@ -16,10 +16,9 @@ class MainActivity : AppCompatActivity() {
     private fun hideSystemUI() {
         if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.R) {
             window.decorView.apply {
-                systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         or View.SYSTEM_UI_FLAG_FULLSCREEN
                         or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
             }
