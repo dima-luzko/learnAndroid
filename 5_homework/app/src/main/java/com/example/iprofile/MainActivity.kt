2682fe293c0a98ca45f4c56566db07add3a6e472
Дима Лузько <dim.luzko@gmail.com>
2021-03-21 15:03:07 +0300

    Add full screen mode.

---
+++ 5_homework/app/src/main/java/com/example/iprofile/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.iprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        hideSystemUI()
+    }
+
+    @Suppress("DEPRECATION")
+    private fun hideSystemUI() {
+        if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.R) {
+            window.decorView.apply {
+                systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+            }
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+}
