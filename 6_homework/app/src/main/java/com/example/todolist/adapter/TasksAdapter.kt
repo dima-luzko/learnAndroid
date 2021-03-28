c9a604ae87785bd33fba1802bd1a0d0ae513ad82
Дима Лузько <dim.luzko@gmail.com>
2021-03-28 17:33:30 +0300

    add swipe

--- 6_homework/app/src/main/java/com/example/todolist/adapter/TasksAdapter.kt
+++ 6_homework/app/src/main/java/com/example/todolist/adapter/TasksAdapter.kt
@@ -6,22 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.chauthai.swipereveallayout.SwipeRevealLayout
+import com.chauthai.swipereveallayout.ViewBinderHelper
 import com.example.todolist.R
 import com.example.todolist.room.model.Task
 
+
 class TasksAdapter(private val taskList: List<Task>, private val click: (Task) -> Unit) :
     RecyclerView.Adapter<TasksAdapter.ViewHolder>() {
+
+    private val viewBinderHelper = ViewBinderHelper()
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val icon: ImageView = view.findViewById(R.id.icon_for_task_in_description_screen)
         val taskName: TextView = view.findViewById(R.id.text_tasks_in_description_screen)
         val taskButton: CardView = view.findViewById(R.id.task_category)
-//        val swipe :SwipeRevealLayout = view.findViewById(R.id.swipe)
+        val swipe :SwipeRevealLayout = view.findViewById(R.id.swipe)
 //        val textEdit : TextView = view.findViewById(R.id.edit_button)
-//        val textDelete : TextView = view.findViewById(R.id.delete_button)
+        val deleteButton : TextView = view.findViewById(R.id.delete_button)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -41,7 +46,11 @@ class TasksAdapter(private val taskList: List<Task>, private val click: (Task) -
             taskButton.setOnClickListener {
                 click(task)
             }
+            deleteButton.setOnClickListener {
+                click(task)
+            }
         }
+        viewBinderHelper.bind(holder.swipe,task.uid.toString())
     }
 
     private fun deleteItem(pos: Int) {
