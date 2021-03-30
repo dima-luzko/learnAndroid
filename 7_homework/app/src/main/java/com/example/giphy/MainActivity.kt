a474260c7ff1446661f92d1231b880163aa3c30f
Дима Лузько <dim.luzko@gmail.com>
2021-03-30 22:03:33 +0300

    add retrofit logic

--- 7_homework/app/src/main/java/com/example/giphy/MainActivity.kt
+++ 7_homework/app/src/main/java/com/example/giphy/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.giphy
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.giphy.adapter.GifAdapter
 import com.example.giphy.data.Gif
 import com.example.giphy.retrofit.common.Common
@@ -17,120 +19,92 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
 
     private var isFirsPressed: Boolean = true
-    private lateinit var gifService: APIGif
-    private lateinit var gifList : List<Gif>
-    private lateinit var layoutManager: LinearLayoutManager
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //hideSystemUI()
-
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        gifService = Common.retrofitService
-        layoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = layoutManager
-        hs()
-
-//        addToRecyclerViewAsGridLayoutManager()
-//        changeRecyclerViewManager()
+        hideSystemUI()
+        addToRecyclerViewAsGridLayoutManager()
+        changeRecyclerViewManager()
     }
 
-//    private fun gifList() {
-//        gifService.getGifList().enqueue(object : Callback<List<Gif>> {
-//            override fun onFailure(call: Call<List<Gif>>, t: Throwable) {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun onResponse(call: Call<List<Gif>>, response: Response<List<Gif>>) {
-//
-//            }
-//        })
-//    }
+    private fun changeRecyclerViewManager() {
+        val menuButton = findViewById<ImageView>(R.id.menu_button)
+        menuButton.setOnClickListener {
+            isFirsPressed = if (isFirsPressed) {
+                menuButton.setImageResource(R.drawable.icon_black_grid_menu)
+                addToRecyclerViewAsLinearLayoutManager()
+                false
+            } else {
+                menuButton.setImageResource(R.drawable.icon_black_vertical_menu)
+                addToRecyclerViewAsGridLayoutManager()
+                true
+            }
+        }
+    }
 
-    private fun hs(){
-        val adapter: GifAdapter
+
+    private fun addToRecyclerViewAsGridLayoutManager() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        val gifService: APIGif = Common.retrofitService
+
         gifService.getGifList().enqueue(object : Callback<List<Gif>> {
             override fun onFailure(call: Call<List<Gif>>, t: Throwable) {
-
             }
 
             override fun onResponse(call: Call<List<Gif>>, response: Response<List<Gif>>) {
-                recyclerView.adapter = GifAdapter(response.body() as List<Gif>)
-
-//                recyclerView.adapter = adapter
-                //adapter.notifyDataSetChanged()
+                with(recyclerView) {
+                    layoutManager = StaggeredGridLayoutManager(
+                        3,
+                        LinearLayoutManager.VERTICAL
+                    )
+                    adapter = GifAdapter(response.body() as List<Gif>)
+                    hasFixedSize()
+                }
             }
         })
     }
+
+    private fun addToRecyclerViewAsLinearLayoutManager() {
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        val gifService: APIGif = Common.retrofitService
+
+        gifService.getGifList().enqueue(object : Callback<List<Gif>> {
+            override fun onFailure(call: Call<List<Gif>>, t: Throwable) {
+            }
+
+            override fun onResponse(call: Call<List<Gif>>, response: Response<List<Gif>>) {
+                with(recyclerView) {
+                    layoutManager = LinearLayoutManager(
+                        this@MainActivity,
+                        LinearLayoutManager.VERTICAL,
+                        false
+                    )
+                    adapter = GifAdapter(response.body() as List<Gif>)
+                    hasFixedSize()
+                }
+            }
+        })
     }
 
-//    private fun changeRecyclerViewManager() {
-//        val menuButton = findViewById<ImageView>(R.id.menu_button)
-//        menuButton.setOnClickListener {
-//            isFirsPressed = if (isFirsPressed) {
-//                menuButton.setImageResource(R.drawable.icon_black_grid_menu)
-//                addToRecyclerViewAsLinearLayoutManager()
-//                false
-//            } else {
-//                menuButton.setImageResource(R.drawable.icon_black_vertical_menu)
-//                addToRecyclerViewAsGridLayoutManager()
-//                true
-//            }
-//        }
-//    }
-//
-//    private fun addToRecyclerViewAsGridLayoutManager() {
-//        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-//        with(recyclerView) {
-//            layoutManager = StaggeredGridLayoutManager(
-//                3,
-//                LinearLayoutManager.VERTICAL
-//            )
-//            adapter = GifAdapter()
-//            hasFixedSize()
-//        }
-//    }
-//
-//    private fun addToRecyclerViewAsLinearLayoutManager() {
-//        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-//        with(recyclerView) {
-//            layoutManager = LinearLayoutManager(
-//                this@MainActivity,
-//                LinearLayoutManager.VERTICAL,
-//                false
-//            )
-//            adapter = GifAdapter(gifList())
-//            hasFixedSize()
-//        }
-//    }
+    @Suppress("DEPRECATION")
+    private fun hideSystemUI() {
+        if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.R) {
+            window.decorView.apply {
+                systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+            }
+        }
+    }
 
-//    private fun gifList() = listOf(
-//        Gif(
-//            gif =
-//        ),
-//        Gif(
-//            gif = R.drawable.sun
-//        )
-//    )
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+}
 
-//    @Suppress("DEPRECATION")
-//    private fun hideSystemUI() {
-//        if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.R) {
-//            window.decorView.apply {
-//                systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                        or View.SYSTEM_UI_FLAG_FULLSCREEN
-//                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
-//            }
-//        }
-//    }
-//
-//    override fun onWindowFocusChanged(hasFocus: Boolean) {
-//        super.onWindowFocusChanged(hasFocus)
-//        if (hasFocus) hideSystemUI()
-//    }
-//}
+
+
--- 7_homework/app/src/main/java/com/example/giphy/data/Gif.kt
+++ 7_homework/app/src/main/java/com/example/giphy/data/Gif.kt
@@ -1,7 +1,11 @@
 package com.example.giphy.data
 
-import androidx.annotation.DrawableRes
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 
 data class Gif (
+
+  @Expose
+  @SerializedName("imageurl")
   val gif: String? = null
 )
